d2c90710bbde0dee5b54ed24bb3c1deeb693de3c
ramu4289 <53053487+ramu4289@users.noreply.github.com>
2019-10-14 21:32:37 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GasMilageTracker
+{
+    public class Program
+    {
+        //public static void Main(string[] args)
+        //{
+        //    BuildWebHost(args).Run();
+        //}
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //WebHost.CreateDefaultBuilder(args)
+        //.UseStartup<Startup>()
+        //.UseDefaultServiceProvider(options =>
+        //options.ValidateScopes = false)
+        //.Build();
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
